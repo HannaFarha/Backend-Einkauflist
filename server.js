00df5b73b471ff5ea3b589cf28fa420458c7df12
HannaFarha <johnnyfarha1@gmail.com>
2024-10-10 19:28:17 +0200

    Vercel

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 //const { config } = require('dotenv')
 require('dotenv').config()
 let corsOptions = {
-   origin : ['https://todolist-hannafarha.netlify.app/'],
+   origin : ['https://todolist-hannafarha.netlify.app'],
 }
 
 app.use(cors(corsOptions))

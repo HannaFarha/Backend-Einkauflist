e8c1813724ba0362d23212a71d31506e5c135dff
HannaFarha <johnnyfarha1@gmail.com>
2024-10-14 17:30:18 +0200

    with out timeout

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 //const { config } = require('dotenv')
 require('dotenv').config()
 let corsOptions = {
-   origin : ['https://wrap4friend-hannafarha.netlify.app/'],
+   origin : ['https://wrap4friend-hannafarha.netlify.app'],
 }
 
 app.use(cors(corsOptions))
